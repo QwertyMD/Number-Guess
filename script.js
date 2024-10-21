b7bbfa436a3b3ba32b3a5f9a972b37436d636a33
Aaryan Dhakal <127584885+QwertyMD@users.noreply.github.com>
2024-10-21 06:43:52 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -18,20 +18,23 @@ function ePlay() {
     hint.innerHTML = `<h2>Guess Something</h2>`;
     return;
   }
-  if (num < input) {
-    hint.innerHTML = `<h2>Think Lower</h2>`;
-  } else if (num > input) {
-    hint.innerHTML = `<h2>Think Higher</h2>`;
-  } else {
-    hint.innerHTML = `<h2>Correct</h2>
-                        <button id="again">Play Again</button>`;
-    const again = document.getElementById("again");
-    again.addEventListener("click", () => {
-      hint.innerHTML = "";
-      document.getElementById("input").value = "";
-      play();
-    });
-  }
+  hint.innerHTML = `<h2>Checking...</h2>`;
+  setTimeout(() => {
+    if (num < input) {
+      hint.innerHTML = `<h2>Think Lower</h2>`;
+    } else if (num > input) {
+      hint.innerHTML = `<h2>Think Higher</h2>`;
+    } else {
+      hint.innerHTML = `<h2>Correct</h2>
+                          <button id="again">Play Again</button>`;
+      const again = document.getElementById("again");
+      again.addEventListener("click", () => {
+        hint.innerHTML = "";
+        document.getElementById("input").value = "";
+        play();
+      });
+    }
+  }, 1000);
 }
 
 play();
